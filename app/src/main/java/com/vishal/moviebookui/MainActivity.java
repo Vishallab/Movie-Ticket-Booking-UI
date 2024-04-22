d4da15fbaf97edec691d1b811a8f19eaba60cf7a
VISHAL MISHRA <Ncepvishal@gmail.com>
2024-04-22 20:00:16 +0530

    intial commit

---
+++ app/src/main/java/com/vishal/moviebookui/MainActivity.java
@@ -0,0 +1,17 @@
+package com.vishal.moviebookui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //here i do not do any code just because currenlty i dont the full code and the logic behihd that all the application is using
+        //so just ii create a normal UI for my practice only
+        //thaks for looking my code thank you have a good day
+    }
+}
